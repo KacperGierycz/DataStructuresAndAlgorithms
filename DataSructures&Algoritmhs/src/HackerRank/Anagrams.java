bc7b247c3a9d40c77ef2d3878fe7e3c0f2816710
KacperGierycz <kacpergierycz@gmail.com>
2021-10-01 16:10:47 +0200

    anagrams done hash maps practice

---
+++ DataSructures&Algoritmhs/src/HackerRank/Anagrams.java
@@ -0,0 +1,123 @@
+package HackerRank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'makeAnagram' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING a
+     *  2. STRING b
+     */
+
+    public static int makeAnagram(String a, String b) {
+    
+    HashMap<Character, Integer>mapOfLetersA= new HashMap<Character,Integer>();
+
+    HashMap<Character, Integer>mapOfLetersB= new HashMap<Character,Integer>();
+    HashMap<Character, Integer>mapOfLetersX= new HashMap<Character,Integer>();
+    int sa=a.length();
+    int sb=b.length();
+    
+     for(int i=0;i<a.length();i++){
+         char charA= a.charAt(i);
+         if (mapOfLetersA.get(charA)==null){
+             mapOfLetersA.put(charA,1);
+         }
+         else{
+             mapOfLetersA.put(charA,mapOfLetersA.get(charA)+1);
+         }
+         
+     }
+     
+          for(int i=0;i<b.length();i++){
+         char charA= b.charAt(i);
+         if (mapOfLetersB.get(charA)==null){
+             mapOfLetersB.put(charA,1);
+         }
+         else{
+             mapOfLetersB.put(charA,mapOfLetersB.get(charA)+1);
+         }
+         
+     }
+     
+     for (Map.Entry<Character,Integer> set:mapOfLetersA.entrySet()){
+         
+    	 Character gg=set.getKey();
+         char aa=set.getKey();
+         int howmany=set.getValue();
+         
+         if (mapOfLetersB.containsKey(aa)){
+             if(mapOfLetersA.get(aa)>mapOfLetersB.get(aa)){
+            	 mapOfLetersX.put(aa,mapOfLetersA.get(aa)-mapOfLetersB.get(aa));
+             }
+             if(mapOfLetersA.get(aa)<mapOfLetersB.get(aa)){
+            	 mapOfLetersX.put(aa,mapOfLetersB.get(aa)-mapOfLetersA.get(aa));
+             }
+             if(mapOfLetersA.get(aa).equals(mapOfLetersB.get(aa))){
+            	 mapOfLetersX.put(aa,0);
+             }
+         }
+         else{
+        	 mapOfLetersX.put(aa,mapOfLetersA.get(aa));
+         }
+         
+     }
+     
+     for (Map.Entry<Character,Integer> set:mapOfLetersB.entrySet()){
+         char aa=set.getKey();
+         int howmany=set.getValue();
+         
+     if(!mapOfLetersX.containsKey(aa)){
+         mapOfLetersX.put(aa, mapOfLetersB.get(aa));
+     }
+        
+    }
+    
+    int ans=0;
+    
+    for (Map.Entry<Character,Integer> set:mapOfLetersX.entrySet()){
+    
+    ans=ans+set.getValue();
+    
+        
+    }
+    return ans;
+    }
+    
+    
+}
+
+public class Anagrams {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+   //    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+       BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String a = bufferedReader.readLine();
+
+        String b = bufferedReader.readLine();
+
+        int res = Result.makeAnagram(a, b);
+      //  System.out.println(res);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- DataSructures&Algoritmhs/src/dificultyEasy/scanner.java
+++ DataSructures&Algoritmhs/src/dificultyEasy/scanner.java
@@ -15,50 +15,7 @@ public class scanner {
 //	str.concat('s');
 //    String chr=str.substring(i,i+1);
 
-    public static String StringChallenge(String str) {
-
-        int l=str.length;
-        StringBuilder nStr = new StringBuilder();
-        String nstr2=new String;
-
-        Char M ='M';
-        Char N ='N';
-
-        // M dup prev char rem M
-        // N dup next char rem N  
-
-       for (int i=0; i<l;i++){
-
-         curr char = str.charAt(i);
-         String chr=str.substring(i,i+1);
-         if (char.equals(M){
-           if (i==0){
-             nstr2.concat(substring(i+1,i+2));
-           }
-          else{
-            nstr2.concat(str.substring(i-1,i));
-          }
-         }
-          if (chr.equals(N){
-            if (i==l-1){
-              breake;
-
-            }
-            else{
-              nstr2.concat(substring(i+1,i+2));
-            }
-          }
-         else{
-           nstr.concat(substring(i+1,i+2));
-         }
-
-       }
-      
-
-        // if(!char.equals(M)||(char.equals(N)) {
-
-        return str;
-      }
+  
 	
 	}
 	
