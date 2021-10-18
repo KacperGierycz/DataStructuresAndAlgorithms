d8b9a839e735cd2122fc14d015e435503dc52cbd
KacperGierycz <kacpergierycz@gmail.com>
2021-10-18 10:39:18 +0200

    javaLoopsII

---
+++ DataSructures&Algoritmhs/src/HackerRank/JavaLoopsII.java
@@ -0,0 +1,27 @@
+package HackerRank;
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int ans=a+b;
+            System.out.print(ans+" ");
+            for(int j=1;j<n;j++){
+                ans+=Math.pow(2,j)*b;
+                System.out.print(ans+" ");
+            }
+            System.out.println();
+            
+        }
+        in.close();
+    }
+}
+
