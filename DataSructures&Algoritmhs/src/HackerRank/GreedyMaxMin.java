c8e58f928198a0c6b51192c786c3aebbd958aedd
KacperGierycz <kacpergierycz@gmail.com>
2021-11-26 14:50:52 +0100

    min max done with pointers

---
+++ DataSructures&Algoritmhs/src/HackerRank/GreedyMaxMin.java
@@ -0,0 +1,82 @@
+package HackerRank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result19 {
+
+    /*
+     * Complete the 'maxMin' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int maxMin(int k, List<Integer> arr) {
+    	
+    	Collections.sort(arr);
+    	System.out.println(arr);
+    	
+    	//smallest from first
+    //	Integer unfairnes=arr.get(k-1)-arr.get(0);
+    	Integer unfairnes=1000000000;
+    	
+    	for(int i=0;i+k<=arr.size();i++) {
+    		
+    		Integer s=arr.get(i);
+    		Integer m=arr.get(i+k-1);
+    		if(m-s<unfairnes)unfairnes=m-s;
+    		
+    	}
+    	
+    	
+    	
+    	
+		return unfairnes;
+    // Write your code here
+
+    }
+
+}
+
+public class GreedyMaxMin {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //   BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result19.maxMin(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
